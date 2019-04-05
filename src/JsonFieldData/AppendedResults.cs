1ba71d0a4fc19c76f479be4fc85683e5b4c9b712
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2019-04-05 14:56:13 -0700

    Issue-1 - Initial commit of JSON parser

---
+++ src/JsonFieldData/AppendedResults.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using FieldDataPluginFramework.Context;
+
+namespace JsonFieldData
+{
+    public class AppendedResults
+    {
+        public string FrameworkAssemblyQualifiedName { get; set; }
+        public string PluginAssemblyQualifiedTypeName { get; set; }
+        public List<FieldVisitInfo> AppendedVisits { get; set; } = new List<FieldVisitInfo>();
+    }
+}
